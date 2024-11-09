9b9a696a0c6c2f753920cc0d5d2a0b171cd85202
Peter Hintze <61521359+coder-guy-pete@users.noreply.github.com>
2024-11-09 17:42:16 -0500

    new recipe modal styled & submit button is working (#32)

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -13,29 +13,51 @@ const addInstructionBtn = document.getElementById('add-instruction-btn');
 function createIngredientField() {
     const ingredientItem = document.createElement('div');
     ingredientItem.classList.add('ingredient-item');
-
+  
     const ingredientNameInput = document.createElement('input');
     ingredientNameInput.setAttribute('type', 'text');
     ingredientNameInput.name = 'ingredients[]';
     ingredientNameInput.placeholder = 'Ingredient Name';
     ingredientNameInput.required = true;
-
+  
     const quantityInput = document.createElement('input');
     quantityInput.setAttribute('type', 'number');
     quantityInput.name = 'ingredients[].quantity';
     quantityInput.placeholder = 'Quantity';
     quantityInput.required = true;
+  
+    const unitOfMeasureSelect = document.createElement('select');
+    unitOfMeasureSelect.name = 'ingredients[].unitOfMeasure';
 
-    const unitOfMeasureInput = document.createElement('input');
-    unitOfMeasureInput.setAttribute('type', 'text');
-    unitOfMeasureInput.name = 'ingredients[].unitOfMeasure';
-    unitOfMeasureInput.placeholder = 'Unit of Measure';
-    unitOfMeasureInput.required = true;
+    const unitOptions = [
+        {value: null, text: 'none'},
+        {value: 'grams', text: 'g'},
+        {value: 'ounces', text: 'oz'},
+        {value: 'pounds', text: 'lbs'},
+        {value: 'cups', text: 'c'},
+        {value: 'pints', text: 'pt'},
+        {value: 'quarts', text: 'qt'},
+        {value: 'gallons', text: 'gal'},
+        {value: 'fluid ounces', text: 'fl oz'},
+        {value: 'milliliters', text: 'ml'},
+        {value: 'liters', text: 'l'},
+        {value: 'teaspoons', text: 'tsp'},
+        {value: 'tablespoons', text: 'tbsp'},
+        {value: 'pieces', text: 'pieces'},
 
+    ];
+  
+    unitOptions.forEach((option) => {
+        const optionElement = document.createElement('option');
+        optionElement.value = option.value;
+        optionElement.textContent = option.text;
+        unitOfMeasureSelect.appendChild(optionElement);
+    });
+  
     ingredientItem.appendChild(ingredientNameInput);
     ingredientItem.appendChild(quantityInput);
-    ingredientItem.appendChild(unitOfMeasureInput);
-
+    ingredientItem.appendChild(unitOfMeasureSelect);
+  
     ingredientList.appendChild(ingredientItem);
 }
 
@@ -61,6 +83,15 @@ function createInstructionField() {
     instructionList.appendChild(instructionItem);
 }
 
+function getFormattedDate() {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+    const yyyy = today.getFullYear();   
+  
+    return mm + '-' + dd + '-' + yyyy;
+  }
+
 addRecipeBtn.addEventListener('click', () => {
     recipeModal.classList.remove('hidden');
 });
@@ -78,24 +109,28 @@ recipeForm.addEventListener('submit', (event) => {
     const formData = new FormData(recipeForm);
     const newRecipe = {};
 
+    newRecipe.dateAdded = getFormattedDate();
+
     for (const [key, value] of formData.entries()) {
-        newRecipe[key] = value;
+        if (key !== 'dateAdded') {
+            newRecipe[key] = value;
+        }
     }
 
-    recipeData.ingredients = [];
+    newRecipe.ingredients = [];
     const ingredientsInputs = document.querySelectorAll('#ingredient-list input');
     for (let i = 0; i < ingredientsInputs.length; i += 3) {
-        recipeData.ingredients.push({
+        newRecipe.ingredients.push({
             ingredientName: ingredientsInputs[i].value,
             quantity: ingredientsInputs[i + 1].value,
             unitOfMeasure: ingredientsInputs[i + 2].value
         });
     }
 
-    recipeData.instructions = [];
+    newRecipe.instructions = [];
     const instructionsInputs = document.querySelectorAll('#instruction-list input');
     instructionsInputs.forEach((input, index) => {
-        recipeData.instructions.push({
+        newRecipe.instructions.push({
             step: index + 1,
             stepDescription: input.value
         });

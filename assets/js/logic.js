1c8953b928885cb238eff32e9c651ec9401edb6d
coder-guy-pete <coder-guy-pete@users.noreply.github.com>
2024-11-11 14:38:53 +0000

    deploy: 5c19b3475d289d082eff6d2751df13b61772356c

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -285,17 +285,3 @@ function renderRecipes() {
 }
 
 renderRecipes();
-
-document.addEventListener("DOMContentLoaded", function () {
-    const plannerCards = document.querySelectorAll(".weekly-planner div");
-  
-    plannerCards.forEach((card) => {
-      card.addEventListener("touchstart", () => {
-        card.classList.add("move");
-        setTimeout(() => {
-          card.classList.remove("move");
-        }, 300); // Reset after animation duration
-      });
-    });
-  });
-  

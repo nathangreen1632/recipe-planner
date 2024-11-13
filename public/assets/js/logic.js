9a8fcd93c12aa951298e68d9d3eba51a04601546
Peter Hintze <61521359+coder-guy-pete@users.noreply.github.com>
2024-11-12 20:33:40 -0500

    grocery list button added (#45)

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -410,37 +410,17 @@ randomRecipeBtn.addEventListener('click', () => {
 });
 
 function addToGroceryList(recipe) {
+    localStorage.removeItem('groceryListArray');
+    localStorage.removeItem('recipeServings');
+
     const groceryList = new Set(JSON.parse(localStorage.getItem('groceryListArray')) || []);
 
     recipe.ingredients.forEach(ingredient => {
     groceryList.add(ingredient);
     });
 
+    localStorage.setItem('recipeServings', recipe.servings);
     localStorage.setItem('groceryListArray', JSON.stringify([...groceryList]));
     
     console.log('Ingredients added to grocery list:', groceryList);
 }
-
-//The following functions are not currently working and likely need to be moved to a separate file
-
-// function renderGroceryList() {
-//     const groceryListContainer = document.querySelector('.grocery-list');
-//     groceryListContainer.innerHTML = ''; // Clear existing content
-
-//     const storedGroceryList = JSON.parse(localStorage.getItem('groceryListArray')) || [];
-    
-//     storedGroceryList.forEach(item => {
-//         const listItem = document.createElement('li');
-//         listItem.textContent = `${item.quantity} ${item.unitOfMeasure} ${item.ingredientName}`;
-//         groceryListContainer.appendChild(listItem);
-//     });
-// }
-
-// function removeItemFromGroceryList(item) {
-//     const groceryList = JSON.parse(localStorage.getItem('groceryListArray')) || [];
-//     const index = groceryList.indexOf(item);
-//     if (index !== -1) {
-//         groceryList.splice(index, 1);
-//         localStorage.setItem('groceryListArray', JSON.stringify(groceryList));
-//     }
-// }
